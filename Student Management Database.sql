885f3b4b3a5d4d8ddd995e87cc0485d5226703ca
Anchal Kumari <kumarianchal0013@gmail.com>
2026-05-06 18:04:42 +0530

    Update Student Management Database.sql

--- Student Management Database.sql
+++ Student Management Database.sql
@@ -183,7 +183,7 @@ JOIN Students s ON e.student_id = s.StudentID
 GROUP BY s.Name
 HAVING COUNT(e.course_id) > 1;
 
--- End of Task 4.
+-- End of Task 3
 
 -- Task-4 (Analytical Reports).
 -- 1. Average Grade by Gender
